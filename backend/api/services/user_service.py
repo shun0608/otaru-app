380d13654d7feab3fa7e625e5edb6770fb5c5107
shun0608 <bmss.4124@icloud.com>
2024-08-12 19:41:19 +0900

    認証処理のロジックをservices/user.service.pyを作成し、記述

---
+++ backend/api/services/user_service.py
@@ -0,0 +1,48 @@
+import os
+
+from fastapi import Depends, Request
+from fastapi_users import BaseUserManager, FastAPIUsers, IntegerIDMixin
+from fastapi_users.db import SQLAlchemyUserDatabase
+from fastapi_users.authentication import (
+    AuthenticationBackend,
+    CookieTransport,
+    JWTStrategy,
+)
+
+from api.db import User, get_user_db
+
+SECRET = os.getenv("SECRET")
+
+
+class UserManager(
+    IntegerIDMixin,
+    BaseUserManager[User, int],
+):
+
+    async def on_after_register(self, user: User, request: Request | None = None):
+        print(f"User {user.id} has registered.")
+
+    async def on_after_delete(self, user: User, request: Request | None = None):
+        print(f"User {user.id} is successfully deleted")
+
+
+async def get_user_manager(user_db: SQLAlchemyUserDatabase = Depends(get_user_db)):
+    yield UserManager(user_db)
+
+
+cookie_transport = CookieTransport(cookie_max_age=3600)
+
+
+def get_jwt_strategy() -> JWTStrategy:
+    return JWTStrategy(secret=SECRET, lifetime_seconds=3600)
+
+
+auth_backend = AuthenticationBackend(
+    name="jwt",
+    transport=cookie_transport,
+    get_strategy=get_jwt_strategy,
+)
+
+fastapi_users = FastAPIUsers[User, int](get_user_manager, [auth_backend])
+
+current_active_user = fastapi_users.current_user(active=True)
